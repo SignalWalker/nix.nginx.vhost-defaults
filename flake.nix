2a32688c631db17d7034c7dedcf8447bc91beb03
Ash Walker <ashurstwalker@gmail.com>
2024-12-06 11:10:57 -0500

    fix: remove formatter input

--- flake.nix
+++ flake.nix
@@ -1,17 +1,12 @@
 {
   description = "A NixOS module for easily blocking user agents from Nginx.";
   inputs = {
-    alejandra = {
-      url = "github:kamadorueda/alejandra";
-    };
     ai-robots-txt = {
       url = "github:ai-robots-txt/ai.robots.txt";
       flake = false;
     };
   };
-  outputs = inputs @ {self, ...}:
-    with builtins; {
-      formatter = std.mapAttrs (system: pkgs: pkgs.default) inputs.alejandra.packages;
-      nixosModules.default = import ./nixos-module.nix {inherit self;};
-    };
+  outputs = {self, ...}: {
+    nixosModules.default = import ./nixos-module.nix {inherit self;};
+  };
 }
